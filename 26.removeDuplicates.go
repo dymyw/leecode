3ffe254c61665737fe8a4677e6a981b69a3eee0c
dymyw <dymayongwei@163.com>
2021-04-13 11:37:37 +0800

    26.removeDuplicates

---
+++ 26.removeDuplicates.go
@@ -0,0 +1,22 @@
+package dymyw_leecode
+
+func removeDuplicates(nums []int) int {
+	// 双指针：slow
+	slow := 0
+
+	// 明确循环移动的是 fast 指针
+	for fast := 1; fast < len(nums); fast++ {
+		// slow 指针联动条件
+		if nums[slow] != nums[fast] {
+			slow++
+
+			// 值操作
+			if nums[slow] != nums[fast] {
+				nums[slow] = nums[fast]
+			}
+		}
+	}
+
+	// slow 最后指向的就是需要计算的结果
+	return slow + 1
+}
