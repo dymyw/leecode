60beedc435a16b114f32a1077c460fedbdd8e337
dymyw <dymayongwei@163.com>
2021-04-17 09:12:54 +0800

    80.removeDuplicates

---
+++ 80.removeDuplicates.go
@@ -0,0 +1,26 @@
+package dymyw_leecode
+
+// tags: Array
+
+func removeDuplicates(nums []int) int {
+	if len(nums) < 2 {
+		return len(nums)
+	}
+
+	// 双指针
+	slow := 2
+
+	// 循环
+	for fast := 2; fast < len(nums); fast++ {
+		// 计算
+		if nums[fast] != nums[slow - 2] {
+			// 计算
+			nums[slow] = nums[fast]
+			// 联动
+			slow++
+		}
+	}
+
+	// 返回某个指针的状态
+	return slow
+}
