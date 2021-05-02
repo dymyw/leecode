79b91b938c411316385e30794623f89fa8517420
dymyw <dymayongwei@163.com>
2021-05-03 07:25:18 +0800

    refactor: Array

---
+++ DataStructure/Array/Array.go
@@ -0,0 +1,6 @@
+package Array
+
+// 数据结构：Array
+// array := []interface{}
+// array := []int
+// array := []string{}
---
+++ DataStructure/Array/Code/1.twoSum.go
@@ -0,0 +1,23 @@
+package Code
+
+// 数据结构：Array、Map
+// 算法：记忆化搜索
+
+func twoSum(nums []int, target int) []int {
+	// 引入 map
+	maps := make(map[int]int)
+
+	// 循环条件
+	for k, v := range nums {
+		// 记忆化搜索 O(n)
+		if index, exists := maps[target - v]; exists {
+			return []int{index, k}
+		}
+
+		// 记忆
+		maps[v] = k
+	}
+
+	// 返回结果
+	return []int{}
+}
