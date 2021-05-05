a331b48c71a336c979baee00606303d4c2b7f028
dymyw <dymayongwei@163.com>
2021-05-06 06:24:45 +0800

    doc(Array): update comment

--- DataStructure/Array/Array.go
+++ DataStructure/Array/Array.go
@@ -1,6 +1,24 @@
 package Array
 
 // 数据结构：Array
+
+// 特点：
+//	连续的内存空间
+
+// 时间复杂度：
+//	Access：O(1)
+//	Insert：平均 O(n)
+//	Delete：平均 O(n)
+
+// 优点：
+//	根据位置查找
+
+// 适用场景：
+//	到处都是
+
+// 特殊情况：
+//	有序数组可以直接进行二分查找。再次提升查找效率 O(log n)
+
 // array := []interface{}
 // array := []int
 // array := []string{}
