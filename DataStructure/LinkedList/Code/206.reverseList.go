6f8a577123987ebafcabbf6b64b519efcc0ebb82
dymyw <dymayongwei@163.com>
2021-05-07 06:34:59 +0800

    doc(LinkedList): update comment

---
+++ DataStructure/LinkedList/Code/206.reverseList.go
@@ -0,0 +1,39 @@
+package Code
+
+import "dymyw-leecode/DataStructure/LinkedList"
+
+// 数据结构：LinkedList
+// 算法：多指针
+
+// ListNode 别名类型
+type ListNode = LinkedList.ListNode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	// 定义指针
+	var prev *ListNode
+	cur := head
+
+	// 循环条件，递归的指针
+	for cur != nil {
+		// // 指针运算
+		// temp := cur.Next
+		// cur.Next = prev
+
+		// // 联动
+		// prev = cur
+		// cur = temp
+
+		// 简化
+		cur.Next, prev, cur = prev, cur, cur.Next
+	}
+
+	// 返回某个指针
+	return prev
+}
--- DataStructure/LinkedList/LinkedList.go
+++ DataStructure/LinkedList/LinkedList.go
@@ -1,5 +1,28 @@
 package LinkedList
 
+// 数据结构：LinkedList
+
+// 特点：
+//	利用零散的内存空间
+
+// 时间复杂度：
+//	Access：O(n)
+//	Insert：平均 O(1)
+//	Delete：平均 O(1)
+
+// 优点：
+//	改善插入、删除
+
+// 适用场景：
+//	不确定元素的个数时
+
+// 特殊情况：
+//	双向链表
+//	环状链表
+
+// head := &ListNode{0, nil}
+// var prev *ListNode
+
 // ListNode 链表节点
 type ListNode struct {
 	Val interface{}
