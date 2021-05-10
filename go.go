a1c41732e9e13618f8de10b468cea132c0de841c
dymyw <dymayongwei@163.com>
2021-05-11 07:09:29 +0800

    feat: add go.go

---
+++ go.go
@@ -0,0 +1,40 @@
+package dymyw_leecode
+
+// 先定义，再使用
+//	a := 10
+//	a = 20
+
+// 交换两个变量
+//	x, y = y, x
+
+// 循环
+//	for k, v := range nums {
+//		// ...
+//	}
+
+// string -> array
+//	b := []byte(s)
+
+// array to string
+//	s = string(b)
+
+// 循环遍历字符串
+//	for i := 0; i < len(s); i++ {
+//		// 字符操作 s[i]
+//	}
+
+// 最大数
+//	math.MaxInt32
+
+// 求较小值
+//	func min(x, y int) int {
+//		if x < y {
+//			return x
+//		}
+//		return y
+//	}
+
+// 二进制的奇偶判断
+//	if len(s) & 1 == 1 {
+//		return false
+//	}
